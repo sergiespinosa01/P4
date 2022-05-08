6c6a634659c8454726e2c9cbcb73afd72b474833
Javier <jafear11@gmail.com>
2022-05-08 19:16:52 +0200

    Diumenge

--- scripts/wav2lpcc.sh
+++ scripts/wav2lpcc.sh
@@ -15,11 +15,11 @@ cleanup() {
 }
 
 if [[ $# != 3 ]]; then
-   echo "$0 lpcc_order input.wav output.lpcc"
+   echo "$0 lpc_order input.wav output.lpcc"
    exit 1
 fi
 
-lpcc_order=$1
+lpc_order=$1
 inputfile=$2
 outputfile=$3
 
@@ -29,21 +29,24 @@ if [[ $UBUNTU_SPTK == 1 ]]; then
    X2X="sptk x2x"
    FRAME="sptk frame"
    WINDOW="sptk window"
-   LPCC="sptk lpcc"
+   LPC="sptk lpc"
+   LPCC="sptk lpc2c"
 else
    # or install SPTK building it from its source
    X2X="x2x"
    FRAME="frame"
    WINDOW="window"
-   LPCC="lpcc"
+   LPC="lpc"
+   LPCC="lpc2c"
 fi
 
+lpcc_order=`echo "scale=0; (($lpc_order * 1.5)+0.5)/1" | bc -l`
 # Main command for feature extration
 sox $inputfile -t raw -e signed -b 16 - | $X2X +sf | $FRAME -l 240 -p 80 | $WINDOW -l 240 -L 240 |
-	$LPCC -l 240 -m $lpcc_order > $base.lpcc
+   $LPC -l 240 -m $lpc_order | $LPCC -m $lpc_order -M $lpcc_order> $base.lpcc
 
 # Our array files need a header with the number of cols and rows:
-ncol=$((lpc_order+1)) # lpcc p =>  (gain a1 a2 ... ap) 
+ncol=$((lpcc_order+1)) # lpcc p =>  (gain a1 a2 ... ap) 
 nrow=`$X2X +fa < $base.lpcc | wc -l | perl -ne 'print $_/'$ncol', "\n";'`
 
 # Build fmatrix file by placing nrow and ncol in front, and the data after them
--- scripts/wav2mfcc.sh
+++ scripts/wav2mfcc.sh
@@ -40,10 +40,10 @@ fi
 
 # Main command for feature extration
 sox $inputfile -t raw -e signed -b 16 - | $X2X +sf | $FRAME -l 240 -p 80 | $WINDOW -l 240 -L 240 |
-	$MFCC -l 240 -m $mfcc_order > $base.mfcc
+	$MFCC -s 8 -l 240 -m $mfcc_order -n 40 -w 1 > $base.mfcc
 
 # Our array files need a header with the number of cols and rows:
-ncol=$((mfcc_order+1)) # mfcc p =>  (gain a1 a2 ... ap) 
+ncol=$((mfcc_order)) # mfcc p =>  (gain a1 a2 ... ap) 
 nrow=`$X2X +fa < $base.mfcc | wc -l | perl -ne 'print $_/'$ncol', "\n";'`
 
 # Build fmatrix file by placing nrow and ncol in front, and the data after them
